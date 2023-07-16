ad44b098894023fa7078f5a6331be111301d7aee
NathanDjianMartin <ndmroot-git@yahoo.fr>
2023-07-16 21:11:13 +0200

    feat(scrape): add linkedInId param

--- backend/src/scrape/scrape.controller.ts
+++ backend/src/scrape/scrape.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ScrapeService } from 'src/scrape/scrape.service';
 
 @Controller('scrape')
 export class ScrapeController {
   constructor(private readonly scrapeService: ScrapeService) {}
 
-  @Get('linkedin')
-  getLinkedInUserData() {
-    return this.scrapeService.getLinkedInUserInfo();
+  @Get('linkedin/:linkedInId')
+  getLinkedInUserData(@Param('linkedInId') linkedInId: string) {
+    return this.scrapeService.getLinkedInUserInfo(linkedInId);
   }
 }
--- backend/src/scrape/scrape.service.ts
+++ backend/src/scrape/scrape.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
-import puppeteer, { EvaluateFunc } from 'puppeteer';
+import puppeteer from 'puppeteer';
 
 @Injectable()
 export class ScrapeService {
-  async getLinkedInUserInfo() {
+  async getLinkedInUserInfo(linkedinId: string) {
     const nameSelector = 'h1';
     const browser = await puppeteer.launch({
       defaultViewport: null,
     });
     const page = await browser.newPage();
-    await page.goto('https://www.linkedin.com/in/nathan-djian-martin/');
+    await page.goto(`https://www.linkedin.com/in/${linkedinId}/`);
     await page.waitForSelector(nameSelector);
 
     const data = await page.evaluate((nameSelector) => {
@@ -18,7 +18,7 @@ export class ScrapeService {
 
       if (!(nameElement instanceof HTMLHeadingElement)) {
         throw new Error(
-          "nameSelector is not a HTMLHeadingElement, it doesn't have a 'innerText' property",
+          "nameSelector is not a HTMLHeadingElement, it doesn't have an 'innerText' property",
         );
       }
       const name = nameElement.innerText;
