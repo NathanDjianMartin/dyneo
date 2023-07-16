57bdc4d5e313dc90700ee8f54978866d8bb7836b
NathanDjianMartin <ndmroot-git@yahoo.fr>
2023-07-16 13:00:37 +0200

    refactor(scrape): move logic into service

--- backend/src/scrape/scrape.service.ts
+++ backend/src/scrape/scrape.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+import puppeteer from 'puppeteer';
+
 @Injectable()
-export class ScrapeService {}
+export class ScrapeService {
+  async getLinkedInUserInfo() {
+    const nameSelector = 'h1';
+    const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.linkedin.com/in/nathan-djian-martin/');
+    await page.waitForSelector(nameSelector);
+
+    const data = await page.evaluate(() => {
+      const name = document.querySelector(nameSelector)?.innerText;
+      return name;
+    });
+
+    return data;
+  }
+}
