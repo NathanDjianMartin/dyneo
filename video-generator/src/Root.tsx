c5f6d9946fa81d53dfe34438bf7075d04de60711
NathanDjianMartin <ndmroot-git@yahoo.fr>
2023-07-18 07:29:53 +0200

    refactor(video): move content into FullComposition

--- video-generator/src/Root.tsx
+++ video-generator/src/Root.tsx
@@ -1,13 +1,14 @@
 import { Composition } from 'remotion';
-import { MyComposition } from './compositions/HeyTitle';
+
+import { FullComposition } from './compositions/FullComposition';
 
 export const RemotionRoot: React.FC = () => {
 	return (
 		<>
 			<Composition
-				id="MyComp"
-				component={MyComposition}
-				durationInFrames={60}
+				id="FullComposition"
+				component={FullComposition}
+				durationInFrames={417}
 				fps={30}
 				width={1080}
 				height={1920}
---
+++ video-generator/src/compositions/FullComposition.tsx
@@ -0,0 +1,11 @@
+import { HeyTitle } from './HeyTitle';
+import { Music } from './Music';
+
+export const FullComposition = () => {
+	return (
+		<>
+			<HeyTitle />
+			<Music />
+		</>
+	);
+};
