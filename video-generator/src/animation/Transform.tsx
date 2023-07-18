524d213efe9682b22bdec7ac3eaee6f3df2a4918
NathanDjianMartin <ndmroot-git@yahoo.fr>
2023-07-18 21:37:30 +0200

    feat(video): add Transform animation

---
+++ video-generator/src/animation/Transform.tsx
@@ -0,0 +1,59 @@
+import { interpolate } from 'remotion';
+import { useCurrentFrame } from 'remotion';
+import React from 'react';
+import { AbsoluteFill } from 'remotion';
+import { Axis, Position } from '../types';
+
+type TransformProps = {
+	children: React.ReactNode;
+	startFrame: number;
+	endFrame: number;
+	startPosition: Position;
+	endPosition: Position;
+};
+
+export const Transform: React.FC<TransformProps> = ({
+	children,
+	startFrame,
+	endFrame,
+	startPosition,
+	endPosition,
+}) => {
+	const frame = useCurrentFrame();
+
+	if (startFrame >= endFrame) {
+		throw new Error(
+			// TODO: factorize in a new error type
+			'TransformSpring animation endFrame should be greater than startFrame'
+		);
+	}
+
+	function getTranslate(axis: Axis) {
+		const startCoordinate = startPosition[axis];
+		const endCoordinate = endPosition[axis];
+		const interpolatedCoordinate = interpolate(
+			frame,
+			[startFrame, endFrame],
+			[startCoordinate, endCoordinate],
+			{
+				extrapolateRight: 'clamp',
+				extrapolateLeft: 'clamp',
+			}
+		);
+		return `translate${axis.toUpperCase()}(${interpolatedCoordinate}px)`;
+	}
+
+	interpolate;
+
+	return (
+		<AbsoluteFill
+			style={{
+				transform: `${getTranslate('x')} ${getTranslate('y')}`,
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			{children}
+		</AbsoluteFill>
+	);
+};
