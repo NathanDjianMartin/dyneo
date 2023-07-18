d928ee58b5a272a58caf682e00ab8c0c1f652d61
NathanDjianMartin <ndmroot-git@yahoo.fr>
2023-07-18 21:41:04 +0200

    feat(video): add animated background

--- video-generator/src/compositions/FullComposition.tsx
+++ video-generator/src/compositions/FullComposition.tsx
@@ -1,10 +1,12 @@
 import { HeyTitle } from './HeyTitle';
+import { InBackground } from './InBackground';
 import { Music } from './Music';
 
 export const FullComposition = () => {
 	return (
 		<>
 			<HeyTitle />
+			<InBackground />
 			<Music />
 		</>
 	);
---
+++ video-generator/src/compositions/InBackground.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Transform } from '../animation/Transform';
+import { AbsoluteFill } from 'remotion';
+
+export const InBackground: React.FC = () => {
+	const startPosition = { x: 1080, y: 0 };
+	const endPosition = { x: 0, y: 0 };
+	return (
+		<Transform
+			startFrame={31}
+			endFrame={38}
+			startPosition={startPosition}
+			endPosition={endPosition}
+		>
+			<AbsoluteFill
+				style={{
+					backgroundColor: '#112233',
+				}}
+			/>
+		</Transform>
+	);
+};
