871582f0374c01ac067512c0ebe2753446f6bd5a
NathanDjianMartin <ndmroot-git@yahoo.fr>
2023-07-19 20:14:54 +0200

    feat: add props to FullComposition

--- video-generator/src/compositions/FullComposition.tsx
+++ video-generator/src/compositions/FullComposition.tsx
@@ -1,11 +1,19 @@
-import { HeyTitle } from './HeyTitle';
+import { Title } from '../components/title/Title';
 import { InBackground } from './InBackground';
 import { Music } from './Music';
+import { FullCompositionProps } from './fullComposition.types';
 
-export const FullComposition = () => {
+export const FullComposition = ({ name = '{name}' }: FullCompositionProps) => {
 	return (
 		<>
-			<HeyTitle />
+			<Title
+				text={`Hey ${name}`}
+				style={{ color: '#112233' }}
+				startFrame={0}
+				endFrame={10}
+				inDuration={5}
+				outDuration={5}
+			/>
 			<InBackground />
 			<Music />
 		</>
---
+++ video-generator/src/compositions/fullComposition.types.ts
@@ -0,0 +1,3 @@
+export type FullCompositionProps = {
+	name: string;
+};
